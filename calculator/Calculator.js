55fd1cb0c3b465919897927ed82e07d512548c81
vemulakalyani <vemulakalyani508@gmail.com>
2023-10-25 16:44:48 +0530

    commit

---
+++ calculator/Calculator.js
@@ -0,0 +1,13 @@
+
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+function display(value) {
+    document.getElementById("result").value += value;
+}
+function calculate() {
+    var x= document.getElementById("result").value;
+    var y = eval(x);
+    document.getElementById("result").value = y;
+}
